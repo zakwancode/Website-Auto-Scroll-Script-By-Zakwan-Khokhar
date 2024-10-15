78866dea3ab68dbaf999e4e7b63b728046327c4c
Zakwan Khokhar <zakwankhokhar96@outlook.com>
2024-10-16 04:08:43 +0500

    zShama Scroll By Zakwan Khokhar.js

--- zShama Scroll By Zakwan Khokhar.js
+++ zShama Scroll By Zakwan Khokhar.js
@@ -1,6 +1,16 @@
-<html>
-  <body>
-    <!-- Your page content here -->
+
+// For other Websites:
+
+// copy and paste this code between the <head></head> tags on each page of your site.
+
+// For Wordpress:
+
+// Install the Wp-Code extension in wordpress and paste it in the head section.
+
+
+
+
+
     <script>
       let scrollInterval = setInterval(function() {
         window.scrollBy(0, 20);
@@ -17,5 +27,11 @@
         }, 100);
       });
     </script>
-  </body>
-</html>
+
+
+
+// Disclaimer:
+
+// The zShama.com repository is provided 'as is' without any warranty of any kind, express 
+// or implied. In no event shall the authors or copyright holders be liable for any claim, 
+// damages, or other liability arising from the use of this repository or any of its contents."
